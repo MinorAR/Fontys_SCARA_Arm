49e7720ae4cc443725103d3b6bb2f2b58de6bf9c
ruudvd <ruud_dommelen@hotmail.com>
2016-05-12 09:45:15 +0200

    added new capturing and filtering

--- src/pcl_filtering/src/pcl_filtering.cpp
+++ src/pcl_filtering/src/pcl_filtering.cpp
@@ -1,9 +1,9 @@
 #include <ros/ros.h>
 // PCL specific includes
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
+#include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/conversions.h>
+#include <pcl/PCLPointCloud2.h>
 // voxel filter
 #include <pcl/filters/voxel_grid.h>
 // passthrough filter
@@ -19,25 +19,46 @@ ros::Publisher pub;
 void cloud_cb(const pcl::PCLPointCloud2ConstPtr& input)
 {
   ros::NodeHandle nh;
-  pcl::PCLPointCloud2::Ptr cloud_stat (new pcl::PCLPointCloud2);
-  pcl::PCLPointCloud2::Ptr cloud_pass(new pcl::PCLPointCloud2);
-  pcl::PCLPointCloud2::Ptr cloud_voxel(new pcl::PCLPointCloud2);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_converted (new pcl::PointCloud<pcl::PointXYZ> );
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pass (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_stat (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_voxel (new pcl::PointCloud<pcl::PointXYZ>);
 
-  std::cerr << "PointCloud before filtering: " << input->width * input->height << " data points." << std::endl;
+  // convert pointcloud2 to pointxyz
+  pcl::fromPCLPointCloud2(*input, *cloud_converted);
+
+  if (cloud_converted == 0)
+  {
+        std::cerr << "input is empty" << std::endl;
+	return;
+  }
+  else
+  {
+        std::cerr << "cloud filtering start: " << cloud_converted << std::endl;
+  }
+
+  std::cerr << "PointCloud before filtering: " << cloud_converted->width * cloud_converted->height << " data points." << std::endl;
 
   //pass through filter
   float xmin, xmax, ymin, ymax, zmin, zmax;
-  nh.getParam("xmin", xmin);
-  nh.getParam("xmax", xmax);
-  nh.getParam("ymin", ymin);
-  nh.getParam("ymax", ymax);
-  nh.getParam("zmin", zmin);
-  nh.getParam("zmax", zmax);
+  nh.getParam("/pcl_filtering/xmin", xmin);
+  nh.getParam("/pcl_filtering/xmax", xmax);
+  nh.getParam("/pcl_filtering/ymin", ymin);
+  nh.getParam("/pcl_filtering/ymax", ymax);
+  nh.getParam("/pcl_filtering/zmin", zmin);
+  nh.getParam("/pcl_filtering/zmax", zmax);
 
-	 //std::cerr << "xmin: " << xmin << std::endl;
+  pcl::PassThrough<pcl::PointXYZ> pass_through_filter;
+  pass_through_filter.setInputCloud (cloud_converted);
 
-  pcl::PassThrough<pcl::PCLPointCloud2> pass_through_filter;
-  pass_through_filter.setInputCloud (input);
+//  pass_through_filter.setFilterFieldName ("x");
+//  pass_through_filter.setFilterLimits (-1, 1);
+//  pass_through_filter.setFilterFieldName ("y");
+//  pass_through_filter.setFilterLimits (-1, 1);
+//  pass_through_filter.setFilterFieldName ("z");
+//  pass_through_filter.setFilterLimits (0, 3);
+//  pass_through_filter.filter (*cloud_pass);
+
   pass_through_filter.setFilterFieldName ("x");
   pass_through_filter.setFilterLimits (xmin, xmax);
   pass_through_filter.setFilterFieldName ("y");
@@ -46,25 +67,24 @@ void cloud_cb(const pcl::PCLPointCloud2ConstPtr& input)
   pass_through_filter.setFilterLimits (zmin, zmax);
   pass_through_filter.filter (*cloud_pass);
 
-  //voxel grid filter
-  pcl::VoxelGrid<pcl::PCLPointCloud2> voxel;
-  voxel.setInputCloud(cloud_pass);
-  voxel.setLeafSize(0.01f, 0.01f, 0.01f);
-  voxel.filter(*cloud_voxel);
-
   //statistical ourlier removal filter
-  pcl::StatisticalOutlierRemoval<pcl::PCLPointCloud2> stat;
-  stat.setInputCloud (cloud_voxel);
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZ> stat;
+  stat.setInputCloud (cloud_pass);
   stat.setMeanK (50);
   stat.setStddevMulThresh (1.0);
   stat.filter (*cloud_stat);
 
+  //voxel grid filter
+  pcl::VoxelGrid<pcl::PointXYZ> voxel;
+  voxel.setInputCloud(cloud_stat);
+  voxel.setLeafSize(0.01f, 0.01f, 0.01f);
+  voxel.filter(*cloud_voxel);
+
   // convert to default pointcloud template
-  std::cerr << "PointCloud after filtering: " << cloud_stat->width * cloud_stat->height << " data points." << std::endl;
+//  std::cerr << "PointCloud after filtering: " << cloud_stat->width * cloud_stat->height << " data points." << std::endl;
 
   // Publish the data
-  pub.publish(*cloud_stat);
-
+  pub.publish(cloud_voxel);
 }
 
 int main (int argc, char** argv)
@@ -77,8 +97,14 @@ int main (int argc, char** argv)
   ros::Subscriber sub = nh.subscribe("input", 1, cloud_cb);
 
   // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2> ( "output_filtered", 1);
+  pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ> > ( "output_filtered", 1, true);
 
-  // Spin
-  ros::spin ();
+  while (ros::ok())
+  {
+  ros::spinOnce();
+  }
+  std::cerr << "pcl_filtering finished" << std::endl;
+
+  ros::shutdown();
+
 }
