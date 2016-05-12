968719fb65586ab109e37a1d7ff91adca573d8f0
Jeroen Rikken <jeroen.rikken@gmail.com>
2016-05-12 15:03:52 +0200

    Added the dynamixel_hr_ros package and the dynamixel_hr python library to overcome limitations of the dynamixel controllers package

---
+++ devel/share/dynamixel_hr_ros/cmake/dynamixel_hr_ros-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(dynamixel_hr_ros_MESSAGE_FILES "/home/jeroen/Software/scara_arm_ws/src/dynamixel_hr_ros/msg/ChainState.msg;/home/jeroen/Software/scara_arm_ws/src/dynamixel_hr_ros/msg/CommandPosition.msg")
+set(dynamixel_hr_ros_SERVICE_FILES "")
