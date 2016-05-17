bc6fdd3bb37d4851175f15a2404e069b5d53289f
Jeroen Rikken <jeroen.rikken@gmail.com>
2016-05-17 19:12:26 +0200

    Changed PID values & built

--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -263,7 +263,7 @@ if __name__ == '__main__':
             sys.exit(1)
 
         # environment at generation time
-        CMAKE_PREFIX_PATH = '/home/ruud/catkin_ws/devel;/opt/ros/indigo'.split(';')
+        CMAKE_PREFIX_PATH = '/home/jeroen/Software/scara_arm_ws/devel;/opt/ros/indigo'.split(';')
         # prepend current workspace if not already part of CPP
         base_path = os.path.dirname(__file__)
         if base_path not in CMAKE_PREFIX_PATH:
