4ad0884cb0fddc7d48a612842346310bd99f92de
scara_setup_pc <dummy@fontys.nl>
2016-05-27 13:51:31 +0200

    catkin make, updated cmakes

--- devel/share/dynamixel_hr_ros/cmake/dynamixel_hr_ros-msg-extras.cmake
+++ devel/share/dynamixel_hr_ros/cmake/dynamixel_hr_ros-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(dynamixel_hr_ros_MESSAGE_FILES "/home/jeroen/Software/Fontys_SCARA_Arm/src/dynamixel_hr_ros/msg/ChainState.msg;/home/jeroen/Software/Fontys_SCARA_Arm/src/dynamixel_hr_ros/msg/CommandPosition.msg")
+set(dynamixel_hr_ros_MESSAGE_FILES "/media/lectoraat/Files/Fontys_SCARA_Arm/src/dynamixel_hr_ros/msg/ChainState.msg;/media/lectoraat/Files/Fontys_SCARA_Arm/src/dynamixel_hr_ros/msg/CommandPosition.msg")
 set(dynamixel_hr_ros_SERVICE_FILES "")
