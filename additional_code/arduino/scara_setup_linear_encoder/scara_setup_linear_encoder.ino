eab76da8bf5f9f638f600ec6e2100e5255c6e541
Jeroen Rikken <jeroen.rikken@gmail.com>
2016-06-09 14:26:17 +0200

    Added initial version of encoder read to the arduino

--- additional_code/arduino/scara_setup_linear_encoder/scara_setup_linear_encoder.ino
+++ additional_code/arduino/scara_setup_linear_encoder/scara_setup_linear_encoder.ino
@@ -4,6 +4,7 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Empty.h>
+#include <std_msgs/String.h>
 
 #include <Encoder.h>
 
@@ -23,6 +24,18 @@ ros::NodeHandle nh;
 
 std_msgs::Float32 val_msg;
 ros::Publisher val_pub("/scara_setup/linear_encoder/value", &val_msg);
+
+std_msgs::String val_shoulder_msg;
+ros::Publisher val_shoulder_pub("/scara_setup/shoulder/value", &val_shoulder_msg);
+
+std_msgs::Float32 val_elbow_msg;
+ros::Publisher val_elbow_pub("/scara_setup/elbow/value", &val_elbow_msg);
+
+std_msgs::Float32 val_wrist_msg;
+ros::Publisher val_wrist_pub("/scara_setup/wrist/value", &val_wrist_msg);
+
+std_msgs::Float32 load_msg;
+ros::Publisher load_pub("/scara_setup/arduino_load", &load_msg);
 
 std_msgs::Bool cal_msg;
 ros::Publisher cal_pub("/scara_setup/linear_encoder/calibrated", &cal_msg);
@@ -45,15 +58,52 @@ void setup() {
   //Set up publisher
   nh.getHardware()->setBaud(115200);
   nh.initNode();
+  nh.advertise(val_shoulder_pub);
+  nh.advertise(val_elbow_pub);
+  nh.advertise(val_wrist_pub);
+  nh.advertise(load_pub);
   nh.advertise(val_pub);
   nh.advertise(cal_pub);
   nh.advertise(lol_pub);
   nh.advertise(upl_pub);
   nh.subscribe(res_sub);
+
+  Serial2.begin(9600);
+  Serial2.setTimeout(200);
 }
 
+const unsigned long lt = 50; //desired loop time
+unsigned long tdiff = 0;
+char test_str[10];
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long t1 = millis();
+
+  Serial2.write("1");
+  String str = Serial2.readStringUntil('\n');
+  //int val = str.toInt();
+  //val_shoulder_msg.data = (float)val;
+  str.trim();
+  str.toCharArray(test_str, 10);
+  val_shoulder_msg.data = test_str;
+  val_shoulder_pub.publish(&val_shoulder_msg);
+
+  /*Serial2.write("2");
+  str = Serial2.readStringUntil('\n');
+  val = str.toInt();
+  val_elbow_msg.data = val;
+  val_elbow_pub.publish(&val_elbow_msg);*/
+
+  /*Serial2.write("3");
+  str = Serial2.readStringUntil('\n');
+  val = str.toInt();
+  val_wrist_msg.data = val;
+  val_wrist_pub.publish(&val_wrist_msg);*/
+
+  load_msg.data = (tdiff * 100.0) / lt;
+  //load_msg.data = tdiff;
+  load_pub.publish(&load_msg);
+  
   val_msg.data = (lin.read() / 1000000.0);
   val_pub.publish(&val_msg);
 
@@ -67,7 +117,12 @@ void loop() {
   upl_pub.publish(&upl_msg);
   
   nh.spinOnce();
-  delay(20);
+
+  tdiff = millis() - t1;
+  
+  if(tdiff < lt){
+    delay(lt - tdiff);
+  }
 }
 
 void refCb()
